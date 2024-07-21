1c3dfe9612b252ac1e4fa4000e54b75d9751bdf3
Carles Espuñes <cespuess@gmail.com>
2024-07-21 17:29:41 +0200

    swagger configurado con ejemplo para el endpoint login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerOptions } from '../swagger.config';
 import { controllers } from '../controllers';
 
 const api = express.Router();
@@ -8,13 +11,73 @@ const myAdsController = new controllers.MyAds();
 const loginController = new controllers.Login();
 const adsController = new controllers.Ads();
 
-// Login
+// ----------------------- Login ------------------------------
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Login:
+ *      type: object
+ *      properties:
+ *        token:
+ *          type: string
+ *          description: authorization token
+ *      example:
+ *        token: dsfoig324okjtl24
+ *    Error:
+ *      type: object
+ *      properties:
+ *        error:
+ *          type: string
+ *          description: Error message
+ *      example:
+ *        error: wrong password
+ */
+
+/**
+ * @swagger
+ * tags:
+ *    name: Login
+ *    description: Login Endpoints
+ */
+
+/**
+ * @swagger
+ * /api/v1/login:
+ *  get:
+ *    summary: Login
+ *    tags: [Login]
+ *    responses:
+ *      200:
+ *        description: loged
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#components/schemas/Login'
+ *              description: response of Login
+ *
+ *      401:
+ *        description: unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#components/schemas/Error'
+ *              description: unauthorized message
+ *
+ */
 api.get('/login', loginController.index);
 
-// Ads
+// ------------------------ Ads -------------------------------
 api.get('/ads', adsController.index);
 
-// MyAds
+// ------------------------ MyAds -----------------------------
 api.get('/myAds', myAdsController.index);
+
+// ------------------------ Swagger ---------------------------
+function initializeSwagger() {
+  const configSwagger = swaggerJSDoc(swaggerOptions);
+  api.use('/swagger', swaggerUi.serve, swaggerUi.setup(configSwagger));
+}
+initializeSwagger();
 
 export default api;
