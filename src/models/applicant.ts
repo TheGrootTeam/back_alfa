2af23b196a2ce252d05a7dd63121e8b6e9cc192b
DAL <149305694+Darrlop@users.noreply.github.com>
2024-07-25 01:17:57 +0200

    Arreglo estropicio

--- src/models/applicant.ts
+++ src/models/applicant.ts
@@ -1,1 +1,46 @@
 
+//const mongoose = require('mongoose');
+import mongoose, { Schema } from 'mongoose';
+
+//Esquema para empresas de la BBDD
+
+const applicantSchema = new mongoose.Schema({
+  dniCif: {type: String, required: true},
+  password: {type: String, required: true}, //aplicar hash
+  name: {type: String, required: true},
+  mail: {type: String, required: true},  // falta verificación de forma
+  phone: {type: String, required: true},
+  photo: {type: String, required: false}, //url a imagen
+  cv: {type: String, required: true}, //url a documento
+  //Alternativa a cv
+  // cv : [{
+  //   company_name: {type: String, required: true},
+  //   start_date: {type: Date, required: true},
+  //   end_date: {type: Date, required: true},
+  //   role: {type: String, required: false},
+  //   job: {type: Boolean, required: true}, //experiencia laboral o académica
+  //   description: {type: String, required: false},
+  // }],
+  ubication: {type: String, required: false},
+  role: {type: String, required: true}, //presencial - teletrabajo - hibrido
+  typeJob: {type: String, required: true}, //renumerado, no renumerado, voluntariado
+  wantedJob: {type: String, required: true}, //puesto preferido que se busca
+  geographically_mobile: {type: Boolean, required: true},
+  disponibility: {type: Boolean, required: true},
+  preferredOffers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'InternshipOffer',
+    required: false
+  }],
+  suscribedOffers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'InternshipOffer',
+    required: false
+  }] 
+});
+
+const Applicant = mongoose.model('Applicant', applicantSchema);
+//module.exports = Applicant;
+
+export default Applicant;
+
--- src/models/company.ts
+++ src/models/company.ts
@@ -1,1 +1,28 @@
 
+//const mongoose = require('mongoose');
+import mongoose, { Schema } from 'mongoose';
+
+//Esquema para empresas de la BBDD
+
+const companySchema = new mongoose.Schema({
+  dniCif: {type: String, required: true},
+  password: {type: String, required: true}, //aplicar hash
+  name: {type: String, required: true},
+  mail: {type: String, required: true}, 
+  phone: {type: String, required: true}, 
+  sector: {type: String, required: true},
+  ubication: {type: String, required: true},
+  description: {type: String, required: true},
+  publishedOffers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'InternshipOffer',
+    required: false
+  }]
+});
+
+
+
+const Company = mongoose.model('Company', companySchema);
+//module.exports = Company;
+
+export default Company;
