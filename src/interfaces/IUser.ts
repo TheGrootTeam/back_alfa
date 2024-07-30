06e0931d807c7e14c73467c2f22b6339b5cf4aa0
DAL <149305694+Darrlop@users.noreply.github.com>
2024-07-30 19:40:44 +0200

    Subida final pull request

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,3 +1,4 @@
+//NO VÁLIDO: DEBE BORRARSE
 // Código original de Ivette
 import { Document } from 'mongoose';
 import { Model } from 'mongoose';
@@ -12,33 +13,3 @@ export interface IUser extends Document {
 export interface IUserModel extends Model<IUser> {
   hashPassword(password: string): Promise<string>;
 }
-
-
-
-
-// import { Document, Types } from 'mongoose';
-
-// // (Company and Applicant combined in Iuser interfce)
-// export interface IUser extends Document{
-//   dniCif: string;
-//   password: string; // Aplicar hash
-//   name: string;
-//   email: string;
-//   phone: string;
-//   photo?: string; // URL a imagen (Applicant)
-//   cv?: string; // URL a documento (Applicant)
-//   ubication: string;
-//   role?: 'presencial' | 'teletrabajo' | 'hibrido'; // (Applicant)
-//   typeJob?: 'renumerado' | 'no renumerado' | 'voluntariado'; //  (Applicant)
-//   wantedJob?: string; // (Applicant)
-//   geographically_mobile?: boolean; // (Applicant)
-//   disponibility?: boolean; // (Applicant)
-//   // preferredOffers?: string[]; // Array de IDs de Offer (Applicant)
-//   // suscribedOffers?: string[]; // Array de IDs de Offer (Applicant)
-//   preferredOffers?: Types.ObjectId[]; // Array de IDs de Offer (Applicant)
-//   suscribedOffers?: Types.ObjectId[]; // Array de IDs de Offer (Applicant)
-//   sector?: string; // (Company)
-//   description?: string; // (Company)
-//   //publishedOffers?: string[]; // Array de IDs de Offer (Company)
-//   publishedOffers?: Types.ObjectId[]; // Array de IDs de Offer (Company)
-// }
