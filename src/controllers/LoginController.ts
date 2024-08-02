f08c55013b6848516617d7596e63cfc8334c7c6f
DAL <149305694+Darrlop@users.noreply.github.com>
2024-08-02 17:52:49 +0200

    resolviendo cosas

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -8,45 +8,6 @@ import { comparePassword } from '../lib/utils';
 
 export default class LoginController {
 
-  async register(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { dniCif, password, isCompany, email } = req.body;
-
-      // validateFields
-      if (!dniCif || !password || isCompany === undefined || !email) {
-        res.status(400).json({ message: 'All fields are required' });
-        return;
-      }
-
-      // Verify if the user already exists
-      const userExists = await User.findOne({ email });
-      if (userExists) {
-        res.status(400).json({ message: 'User already exists' });
-        return;
-      }
-
-      // Password hash
-      const hashedPassword = await hashPassword(password);
-
-      // Create new user
-      const user = new User({
-        dniCif,
-        password: hashedPassword,
-        isCompany,
-        email
-      });
-
-      await user.save();
-
-      res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  // index(_req: Request, res: Response, _next: NextFunction) {
-  //   res.json({ token: 'Token desde LoginController!' });
-  // }
   async post(req: Request, res: Response, next: NextFunction) {
     try {
       const { dniCif, password } = req.body;
@@ -58,8 +19,6 @@ export default class LoginController {
 
       const user = userApplicant ? userApplicant : userCompany;
 
-      //@ts-ignore
-      console.log('HASH: ', await hashPassword(user.password));
 
       // throw error if don't find the user
 
