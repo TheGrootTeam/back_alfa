49bb382149b171e3e9dd3b5b77ca2ff4f98f9987
Carles Espuñes <cespuess@gmail.com>
2024-08-09 18:59:32 +0200

    models and examples from rol, sector and skill created

--- src/exampleDBjson/index.ts
+++ src/exampleDBjson/index.ts
@@ -2,6 +2,9 @@
 import exOffers from './exOffers.json';
 import exApplicants from './exApplicants.json';
 import exCompanies from './exCompanies.json';
+import exSkills from './exSkills.json';
+import exRols from './exRols.json';
+import exSector from './exSector.json';
 
 //export default { exUsers, exOffers, exApplicants, exCompanies };
-export default { exOffers, exApplicants, exCompanies };
+export default { exOffers, exApplicants, exCompanies, exSkills, exRols, exSector };
