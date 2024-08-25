584025dc21545d55874360f52fef3e5cdef20769
Syra Dominguez <syradominguez.dev@gmail.com>
2024-08-25 14:25:34 +0200

    feat: Update authJWT middleware to handle token with "Bearer" prefix

--- src/middlewares/authJWT.ts
+++ src/middlewares/authJWT.ts
@@ -4,23 +4,31 @@ import { CustomRequest, JwtPayload } from '../interfaces/IauthJWT';
 import jwt, { VerifyErrors } from 'jsonwebtoken';
 
 export default (req: CustomRequest, _res: Response, next: NextFunction) => {
-  // token can be in the header, body or as query string
-  const tokenJWT = req.headers['authorization'] || req.headers['Authorization'];
+  // Get token from header, body, or query string
+  let tokenJWT = req.headers['authorization'] || req.headers['Authorization'];
 
-  // if there is no token throw error using http-errors
+  // Log the token to verify it's being received correctly
+  console.log('Received Token:', tokenJWT);
+
+  // If tokenJWT exists, remove "Bearer" prefix if present
+  if (tokenJWT && typeof tokenJWT === 'string' && tokenJWT.startsWith('Bearer ')) {
+    tokenJWT = tokenJWT.slice(7, tokenJWT.length).trim();
+  }
+
+  // If there is no token, throw an error
   if (!tokenJWT) {
     next(createError(401, 'No token provided'));
     return;
   }
 
-  // verify jwt
+  // Verify JWT
   jwt.verify(tokenJWT, process.env.JWT_SECRET as string, (err: VerifyErrors | null, payload: unknown) => {
     if (err) {
       next(createError(401, 'Invalid token'));
       return;
     }
 
-    // add userid data to req so next middlewares can use the variable
+    // Add userId to req so next middlewares can use the variable
     req.apiUserId = (payload as JwtPayload).userId;
     next();
   });
--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import ProfileController from '../controllers/EditProfileController';
-
+import authJWT from '../middlewares/authJWT'; 
 
 const router = express.Router();
 const profileController = new ProfileController();
 
-router.put('/update', (req, res, next) => profileController.updateProfile(req, res, next));
+// Protect the profile update route with Authjwt
+router.put('/update', authJWT, (req, res, next) => profileController.updateProfile(req, res, next));
 
 export default router;
