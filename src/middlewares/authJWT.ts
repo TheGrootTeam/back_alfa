bd28f50e1966ae2f126ec7a1af271580b650041e
Carles Espuñes <cespuess@gmail.com>
2024-08-26 15:43:04 +0200

    authJWT with bearer separated of token

--- src/middlewares/authJWT.ts
+++ src/middlewares/authJWT.ts
@@ -5,7 +5,13 @@ import jwt, { VerifyErrors } from 'jsonwebtoken';
 
 export default (req: CustomRequest, _res: Response, next: NextFunction) => {
   // token can be in the header, body or as query string
-  const tokenJWT = req.headers['authorization'] || req.headers['Authorization'];
+  const tokenBearer = req.headers['authorization'] || req.headers['Authorization'];
+  let tokenJWT: string | undefined;
+
+  if (tokenBearer) {
+    const tokenparts = tokenBearer.split(' ');
+    tokenJWT = tokenparts[1];
+  }
 
   // if there is no token throw error using http-errors
   if (!tokenJWT) {
