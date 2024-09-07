5c855ec605035e6f5594234739ad813b8f5f26aa
Carles Espuñes <cespuess@gmail.com>
2024-09-07 13:35:13 +0200

    change applicant password

--- src/controllers/passwordController.ts
+++ src/controllers/passwordController.ts
@@ -2,7 +2,7 @@ import { Response, NextFunction } from 'express';
 import createError from 'http-errors';
 import Company from '../models/Company';
 import { CustomRequest } from '../interfaces/IauthJWT';
-// import Applicant from '../models/Applicant';
+import Applicant from '../models/Applicant';
 import { comparePassword, hashPassword } from '../lib/utils';
 
 export default class PasswordController {
@@ -39,6 +39,29 @@ export default class PasswordController {
         const hashedNewPassword = await hashPassword(newPassword);
 
         await Company.findByIdAndUpdate(userId, { password: hashedNewPassword });
+        res.status(200).json({ message: 'Password changed successfully' });
+      } else {
+        const response = await Applicant.findById(userId).select('password');
+        // create error if user not in Company
+        if (!response) {
+          next(createError(400, 'UserId or isCompany not correct'));
+          return;
+        }
+
+        const password = response.password;
+
+        // compare current password
+        const comparedPassword = await comparePassword(currentPassword, password);
+
+        if (!comparedPassword) {
+          next(createError(400, 'Current password not correct'));
+          return;
+        }
+
+        // hash passwords
+        const hashedNewPassword = await hashPassword(newPassword);
+
+        await Applicant.findByIdAndUpdate(userId, { password: hashedNewPassword });
         res.status(200).json({ message: 'Password changed successfully' });
       }
     } catch (error) {
