6214827969be9114202724ce6d14b0e266f154ea
Carles Espuñes <cespuess@gmail.com>
2024-09-12 20:03:49 +0200

    error if email no user

--- src/controllers/LostPasswordController.ts
+++ src/controllers/LostPasswordController.ts
@@ -1,11 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
-import TokenLostPassword from '../models/TokenLostPassword';
+import Company from '../models/Company';
+import Applicant from '../models/Applicant';
+import createError from 'http-errors';
+// import TokenLostPassword from '../models/TokenLostPassword';
 
 export default class LostPasswordController {
-  async index(_req: Request, res: Response, _next: NextFunction) {
-    const newtoken = new TokenLostPassword({ token: 'sdfsd', userId: '66e0af5d73e71202eccae4d5' });
-    const ola = await newtoken.save();
-    console.log(ola);
-    res.status(200).json({ hola: 'hola' });
+  async index(req: Request, res: Response, next: NextFunction) {
+    try {
+      const email = req.params.email;
+
+      let user = await Company.findOne({ email });
+
+      if (!user) {
+        user = await Applicant.findOne({ email });
+      }
+
+      if (user) {
+        // const newtoken = new TokenLostPassword({ token: 'sdfsd', userId: '66e0af5d73e71202eccae4d5' });
+        // const ola = await newtoken.save();
+        // console.log(ola);
+        res.status(200).json({ email });
+      } else {
+        next(createError(404, 'No user with this email'));
+        return;
+      }
+    } catch (error) {
+      next(error);
+    }
   }
 }
--- src/routes/lostPasswordRoutes.ts
+++ src/routes/lostPasswordRoutes.ts
@@ -4,6 +4,6 @@ import LostPasswordController from '../controllers/LostPasswordController';
 const lostPasswordRoutes = express.Router();
 const lostPasswordController = new LostPasswordController();
 
-lostPasswordRoutes.get('/', lostPasswordController.index);
+lostPasswordRoutes.get('/:email', lostPasswordController.index);
 
 export default lostPasswordRoutes;
