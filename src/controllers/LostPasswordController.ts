d530b9d64eff910c7d5bf81c959ef4ad74c14654
Carles Espuñes <cespuess@gmail.com>
2024-09-12 21:04:11 +0200

    verify email and send token to change password

--- src/controllers/LostPasswordController.ts
+++ src/controllers/LostPasswordController.ts
@@ -2,12 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import Company from '../models/Company';
 import Applicant from '../models/Applicant';
 import createError from 'http-errors';
-// import TokenLostPassword from '../models/TokenLostPassword';
+import TokenLostPassword from '../models/TokenLostPassword';
+import jwt from 'jsonwebtoken';
 
 export default class LostPasswordController {
   async index(req: Request, res: Response, next: NextFunction) {
     try {
       const email = req.params.email;
+      let jwtToken = '';
 
       let user = await Company.findOne({ email });
 
@@ -16,10 +18,22 @@ export default class LostPasswordController {
       }
 
       if (user) {
-        // const newtoken = new TokenLostPassword({ token: 'sdfsd', userId: '66e0af5d73e71202eccae4d5' });
-        // const ola = await newtoken.save();
-        // console.log(ola);
-        res.status(200).json({ email });
+        const token = await TokenLostPassword.findOne({ userId: user._id.toString() });
+        // create new jwtToken
+        jwtToken = await jwt.sign({ user: user._id.toString() }, process.env.JWT_SECRET as string, {
+          expiresIn: '1h'
+        });
+
+        if (token) {
+          // modifie token for document if exist
+          await TokenLostPassword.findByIdAndUpdate(token._id, { token: jwtToken });
+        } else {
+          // save token in bbdd
+          const newtoken = new TokenLostPassword({ token: jwtToken, userId: user._id.toString() });
+          await newtoken.save();
+        }
+        const url = `http://${req.host}/lost-password/${jwtToken}`;
+        res.status(200).json({ email, name: user.name, url });
       } else {
         next(createError(404, 'No user with this email'));
         return;
